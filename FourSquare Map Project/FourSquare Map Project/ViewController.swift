555ad96ee15dfc3646e2abaa2280c8e05224d684
Jason Ruan <jasonruan@pursuit.org>
2019-11-06 16:48:56 -0500

    Initial commit

---
+++ FourSquare Map Project/FourSquare Map Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FourSquare Map Project
+//
+//  Created by Jason Ruan on 11/6/19.
+//  Copyright © 2019 Jason Ruan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
