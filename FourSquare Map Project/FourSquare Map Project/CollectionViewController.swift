5a405d8f258aafab1f9a70d1874e85a5760a9a5c
Jason Ruan <jasonruan@pursuit.org>
2019-11-07 16:09:52 -0500

    Adds new view controller files, deletes main.storyboard and references in info.plist, and edits SceneDelegate to have tab bar controller with search and collection VCs

---
+++ FourSquare Map Project/FourSquare Map Project/CollectionViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionViewController.swift
+//  FourSquare Map Project
+//
+//  Created by Jason Ruan on 11/7/19.
+//  Copyright © 2019 Jason Ruan. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+    
+}
