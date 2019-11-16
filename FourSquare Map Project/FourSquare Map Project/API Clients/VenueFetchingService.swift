b6f75573b8fccaac0790ae9a43eaf5d23f1491f6
Jason Ruan <jasonruan@pursuit.org>
2019-11-15 21:57:15 -0500

    Adds api client and model

---
+++ FourSquare Map Project/FourSquare Map Project/API Clients/VenueFetchingService.swift
@@ -0,0 +1,44 @@
+//
+//  VenueFetchingService.swift
+//  FourSquare Map Project
+//
+//  Created by Jason Ruan on 11/15/19.
+//  Copyright © 2019 Jason Ruan. All rights reserved.
+//
+
+import Foundation
+
+class VenueFetchingService {
+    private init() {}
+    static let manager = VenueFetchingService()
+    
+    func getVenues(lat: Double, long: Double, query: String, completionHandler: @escaping (Result<[Venue], AppError>) -> () ){
+        //TODO: Change limit after confirmed working
+        let urlString = "https://api.foursquare.com/v2/venues/explore?client_id=\(Secrets().clientId)&client_secret=\(Secrets().clientSecret)&v=20191106&limit=2&ll=\(lat),\(long)&query=\(query)"
+        
+        guard let url = URL(string: urlString)  else {
+            completionHandler(.failure(.badURL))
+            return
+        }
+        
+        NetworkHelper.manager.performDataTask(withUrl: url, andMethod: .get) { (result) in
+            switch result {
+            case .failure:
+                completionHandler(.failure(.noDataReceived))
+            case .success(let data):
+                do {
+                    let decodedData = try JSONDecoder().decode(LocationResultsWrapper.self, from: data)
+                    if let retrievedVenues = decodedData.response?.groups?.first?.items {
+                        completionHandler(.success(retrievedVenues))
+                    } else {
+                        completionHandler(.failure(.invalidJSONResponse))
+                    }
+                    
+                } catch {
+                    completionHandler(.failure(.couldNotParseJSON(rawError: error)))
+                }
+            }
+        }
+        
+    }
+}
--- FourSquare Map Project/FourSquare Map Project/Models/Venue.swift
+++ FourSquare Map Project/FourSquare Map Project/Models/Venue.swift
@@ -8,6 +8,41 @@
 
 import Foundation
 
-struct Venue {
-    
+struct LocationResultsWrapper: Codable {
+    let response: ResponseWrapper?
+}
+
+struct ResponseWrapper: Codable {
+    let groups: [GroupsWrapper]?
+}
+
+struct GroupsWrapper: Codable {
+    let items: [Venue]?
+}
+
+struct Venue: Codable {
+    let venue: VenueDetails?
+}
+
+struct VenueDetails: Codable {
+    let id: String?
+    let name: String?
+    let location: LocationWrapper?
+    let distance: Double?
+    let neighborhood: String?
+    let city: String?
+    let state: String?
+    let formattedAddress: String?
+    let categories: [CategoriesWrapper]?
+}
+
+struct LocationWrapper: Codable {
+    let lat: Double?
+    let lng: Double?
+}
+
+struct CategoriesWrapper: Codable {
+    let id: String?
+    let name: String?
+    let shortName: String?
 }
